4a6ef46d8d4d37dff762512517ae7c85e3b80010
typical-developer047 <abdullah.ibn.shareef12@gmail.com>
2024-12-01 21:30:00 +0530

    add footer.js

---
+++ client/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ textAlign: 'center', padding: '20px', backgroundColor: '#f1f1f1', position: 'fixed', width: '100%', bottom: 0 }}>
+      <p>Developed by SMSUS</p>
+    </footer>
+  );
+};
+
+export default Footer;
